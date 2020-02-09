a8c189cb2ff460861ca99a8dd5d57c96783abaac
Vitalii Zymukha <unknownvzzv@gmail.com>
2020-02-09 23:14:36 +0200

    Should clean html before filtering

--- src/message_provider/habr_message_provider.rs
+++ src/message_provider/habr_message_provider.rs
@@ -102,14 +102,17 @@ impl CommentProvider for HabrCommentsProvider {
         let comment_opt = comments
             .iter_mut()
             .map(|c| sanitize_html(c.inner_html().borrow_mut()))
-            .filter(|c| { !c.contains("<p>") && !c.contains("<a>") } )
+            .map(|comment| {
+                comment
+                    .replace("<br>", "\n")
+                    .replace("<blockquote>", "»")
+                    .replace("</blockquote>", "")
+                    .replace("<b>", "*")
+                    .replace("</b>", "*")
+            })
+            .filter(|c| { !c.contains("<p>") && !c.contains("<a ") })
             .find(|c| c.len() <= *COMMENT_LEN_CHARS);
 
-        comment_opt.map(|comment| {
-            comment
-                .replace("<br>", "\n")
-                .replace("<blockquote>", ">>")
-                .replace("</blockquote>", "")
-        })
+        comment_opt
     }
 }
